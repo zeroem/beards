069f8e8b8d2dcff780b96be7d1d967dc9aad4c55
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-07 20:19:39 -0700

    big bang

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject beards "0.1.0-SNAPSHOT"
+  :description "An implementation of the Mustache Template Engine"
+  :url "http://github.com/zeroem/beards"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/beards/core.clj
@@ -0,0 +1,6 @@
+(ns beards.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
