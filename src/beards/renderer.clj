558ea21cc384c8de9fbf984f6bfcb62798fc61e0
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-16 23:35:16 -0600

    basic section rendering impl

--- src/beards/renderer.clj
+++ src/beards/renderer.clj
@@ -1,23 +1,68 @@
 (ns beards.renderer)
 
-(defmulti render-token (fn [s cursor token data] (:type token)))
+(declare render)
+(defn make-cursor [token]
+  (->> (clojure.string/split (:expression token) #"\.")
+       (map keyword)))
 
-(defmethod render-token :comment [s cursor token data] [cursor ""])
+(defmulti render-token (fn [state token data] (:type token)))
 
-(defmethod render-token :string [s
-                                 cursor
+(defmethod render-token :comment [state token data] [state ""])
+(defmethod render-token :change-delims [state & _] [state ""])
+
+(defmethod render-token :string [state
                                  {:keys [start-pos end-pos] :as token}
                                  _]
-  [cursor (subs s start-pos end-pos)])
+  [state (subs (:s state) start-pos end-pos)])
 
-(defmethod render-token :default [s cursor token data] [cursor "DEFAULT"])
+(defmethod render-token :lookup [state token data]
+  (let [cursor (make-cursor token)]
+    [state (str (get-in data cursor))]))
+
+(defn capture-section [state token]
+  (let [cursor (make-cursor token)]
+    (loop [exit-state state
+           section-state (assoc state :tokens [])]
+      (if-let [t (first (:tokens exit-state))]
+        (if (= (:type t) :end-section)
+          (if (= (:expression t) (:expression token))
+            [(update-in exit-state [:tokens] rest) section-state]
+            (throw (IllegalArgumentException.
+                    (str "Expected end of `"
+                         (:expression token)
+                         "` but found "
+                         (:expression t)))))
+          (recur (update-in exit-state [:tokens] rest)
+                 (update-in section-state [:tokens] conj t)))
+        (throw (IllegalArgumentException. "EOF before end of section"))))))
+
+(defmethod render-token :start-section [state token data]
+  (let [target (get-in data (make-cursor token))
+        [exit-state section-state] (capture-section state token)]
+    (cond
+     (map? target)
+     [exit-state (render section-state target)]
+
+     ((some-fn vector? list? seq?) target)
+     [exit-state
+      (->> target
+           (map (fn [d] (render section-state d)))
+           (apply str))
+
+      ((some-fn empty? false?) target)
+      [exit-state ""]])))
+
+(defmethod render-token :stop-section [& _]
+  (throw (RuntimeException. "We should never get here.")))
+
+(defmethod render-token :default [state token data] [state "DEFAULT"])
 
 
-(defn render [s tokens data]
-  (loop [tokens tokens
-         cursor []
+(defn render [state data]
+  (loop [state state
          result ""]
-    (if-let [token (first tokens)]
-      (let [[cursor rs] (render-token s cursor token data)]
-        (recur (rest tokens) cursor (str result rs)))
+    (if-let [token (first (:tokens state))]
+      (let [state (update-in state [:tokens] rest)
+            [state rs] (render-token state token data)]
+        (recur state (str result rs)))
       result)))
--- test/beards/renderer_test.clj
+++ test/beards/renderer_test.clj
@@ -1,10 +1,11 @@
 (ns beards.renderer-test
   (:require [clojure.test :refer :all]
-            [beards.renderer :refer :all]))
+            [beards.renderer :refer :all]
+            [beards.parser :refer [parse]]))
 
 
 (defn simple-render [s token]
-  (second (render-token s [] token {})))
+  (second (render-token {:s s} token {})))
 
 (deftest basic-token-rendering
   (testing "comments are blank"
@@ -15,3 +16,22 @@
     (is (= (simple-render "This is a Test"
                           {:type :string :start-pos 0 :end-pos 4})
            "This"))))
+
+(deftest render-test
+  (testing "I can render a collection of tokens"
+    (let [s "This is a {{foo}} string {{!comment}}"
+          state (parse s)
+          result (render state {:foo "bar"})]
+      (is (= result "This is a bar string "))))
+
+  (testing "A Collection section will render"
+    (let [s "{{#foo}}{{bar}}{{/foo}}"
+          state (parse s)
+          result (render state {:foo (take 3 (repeat {:bar "baz"}))})]
+      (is (= result "bazbazbaz"))))
+
+  (testing "A false value will not render"
+    (let [s "{{#foo}}{{bar}}{{/foo}}"
+          state (parse s)
+          result (render state {:foo nil})]
+      (is (= result "")))))
